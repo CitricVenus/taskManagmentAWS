447f5fa64ee54915d2407b5e853462655517bf14
CitricVenus <inquisidor.890@hotmail.com>
2025-06-26 01:05:51 -0600

    update isntall.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -x
 
-DEST=/home/ec2-user/app
+DEST="/home/ec2-user/app"
 
 mkdir -p "$DEST"
-
-# Copia solo el contenido del directorio de despliegue (ya está en build/)
-cp -r * "$DEST/"
+cp -r . "$DEST"

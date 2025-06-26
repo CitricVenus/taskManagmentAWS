2a19b5039cae2c152334221301f703ca03d624e3
CitricVenus <inquisidor.890@hotmail.com>
2025-06-26 01:09:39 -0600

    asd

--- install.sh
+++ install.sh
@@ -3,5 +3,10 @@ set -x
 
 DEST="/home/ec2-user/app"
 
+# Borra contenido previo
+rm -rf "$DEST"
 mkdir -p "$DEST"
-cp -r . "$DEST"
+
+# Copiar desde el directorio actual (paquete CodeDeploy)
+# Solo copiamos los archivos y carpetas esperadas
+cp -r index.html static asset-manifest.json manifest.json robots.txt "$DEST/"

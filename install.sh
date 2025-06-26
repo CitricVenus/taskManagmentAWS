2d5a1f642cd46e56013b59eb1494c18f1a667b74
Erick Alfonso Montan Lopez <inquisidor.890@hotmail.com>
2025-06-25 23:26:15 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -x
 
-DEST=/home/ec2-user/app
-
-mkdir -p "$DEST"
-
-# Copiar todo el contenido del directorio actual (artifact) al destino
-# excluyendo carpetas ocultas (opcional)
-cp -r ./* "$DEST/"
+cd /home/ec2-user/app

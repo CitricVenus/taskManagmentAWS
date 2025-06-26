ab3b24e5c83adcd771ee1d30ec35391170012337
Erick Alfonso Montan Lopez <inquisidor.890@hotmail.com>
2025-06-25 23:14:46 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-cd /home/ec2-user/app
-npx http-server -p 3000 &
+set -x
+
+cd /home/ec2-user/app || exit 1
+
+# Inicia http-server en segundo plano, redirige salida para debug
+nohup npx http-server -p 3000 > http-server.log 2>&1 &

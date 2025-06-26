3d1c351552c57dc88eb44cc5f81387ef4f664175
Erick Alfonso Montan Lopez <inquisidor.890@hotmail.com>
2025-06-25 22:48:13 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-set -x  # Muestra los comandos mientras se ejecutan, útil para debug
-
-# Asegura que npm esté disponible en el entorno del CodeDeploy
-export PATH=$PATH:/usr/bin:/usr/local/bin
-
-# Ir a la carpeta donde se desplegó el código
-cd /home/ec2-user/app
-
-# Instalar dependencias y construir el proyecto
-npm install
-npm run build
+set -x
+mkdir -p /home/ec2-user/app
+cp -r * /home/ec2-user/app/
